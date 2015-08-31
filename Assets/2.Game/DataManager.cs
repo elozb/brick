fe86b733f375b556adafa76de5659b165a429352
elozb <csyluv@nate.com>
2015-08-31 00:32:46 -0800

    0830

--- Assets/2.Game/DataManager.cs
+++ Assets/2.Game/DataManager.cs
@@ -35,10 +35,13 @@ public class DataManager : MonoBehaviour {
 	const float GAMEPANEL_WIDTH = BLOCK_WIDTH * BLOCK_ROW;
 	const float GAMEPANEL_HEIGHT = BLOCK_HEIGHT * (BLOCK_COLUMN + 10);	//하단의 플레이어 유닛부터 블록이 놓일 부분까지 약 10칸..
 
-	int[,] blocks;
+	int[,] brickData;
+
+	GameObject brick;
 	// Use this for initialization
 	void Awake()
 	{
+		brick = Resources.Load ("Prefabs/brick") as GameObject;
 		initBlock ();
 		ArrangeBlock ();
 	}
@@ -54,7 +57,7 @@ public class DataManager : MonoBehaviour {
 	{
 		//차후에 외부 에서 불러오는 형식으로..
 
-		blocks = new int[BLOCK_COLUMN,BLOCK_ROW]{
+		brickData = new int[BLOCK_COLUMN,BLOCK_ROW]{
 					{0,0,0,0,0,0,0,0,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0,0,0,0},
 					{0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -80,15 +83,22 @@ public class DataManager : MonoBehaviour {
 
 	void ArrangeBlock()
 	{
+		//벽돌 배치..
 		for (int i = 0; i<BLOCK_COLUMN; i++) {
 			for (int j = 0; j<BLOCK_ROW; j++) {
-				if(blocks[i][j] == 0)
+				if(blocks[i,j] == 0)
 				{
 					continue;
 				}
 				//0이아닌경우 블록 배치..
 
+				// 실제 인스턴스 생성
 
+				GameObject cloneBrick = MonoBehaviour.Instantiate (brick) as GameObject;
+
+				cloneBrick.name = "brick_"+i.ToString()+"_"+j.ToString; // name을 변경
+				cloneBrick.transform.parent = player.transform;
+				// bullet을 player에 입양하는등 초기화작업 수행
 			}
 		}
 	}
